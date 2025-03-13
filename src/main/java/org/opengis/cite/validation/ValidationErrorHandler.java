c8f0c6faeff0ab9a09664026dd8c9b88f0b211fd
Benjamin Proß <b.pross@52north.org>
2025-03-13 13:41:03 +0100

    Filter validation warning about grammar pools

--- src/main/java/org/opengis/cite/validation/ValidationErrorHandler.java
+++ src/main/java/org/opengis/cite/validation/ValidationErrorHandler.java
@@ -64,7 +64,9 @@ public class ValidationErrorHandler implements ErrorHandler, DOMErrorHandler, It
 	 * @see org.xml.sax.ErrorHandler#warning(org.xml.sax.SAXParseException)
 	 */
 	public void warning(SAXParseException spex) {
-
+	        if(spex.getMessage().equals("One of the grammar(s) returned from the user's grammar pool is in conflict with another grammar.")) {
+	            return;
+	        }
 		addSAXError(ErrorSeverity.WARNING, spex);
 	}
 
